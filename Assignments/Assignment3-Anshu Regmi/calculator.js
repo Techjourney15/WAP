9d859c4ae4b1c590db44d47a79982c792f3023a4
Anshu <regmianshu307@gmail.com>
2026-01-30 19:23:38 +0545

    Added assignment-3

---
+++ Assignments/Assignment3-Anshu Regmi/calculator.js
@@ -0,0 +1,11 @@
+// Arrow function for addition
+export const funcAdd = (a, b) => a + b;
+
+// Arrow function for subtraction
+export const funcSub = (a, b) => a - b;
+
+// Arrow function for multiplication
+export const funcMul = (a, b) => a * b;
+
+// Arrow function for division
+export const funcDiv = (a, b) => a / b;
